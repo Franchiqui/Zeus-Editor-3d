210f4c1d7c82b95fba85c176937a060130022f3d
Franchiqui <exchiqui.fr@gmail.com>
2026-09-24 06:25:23 +0200

    Último cambio 24/09/2026

--- components/ui/slider.tsx
+++ components/ui/slider.tsx
@@ -19,18 +19,18 @@ const Slider = React.forwardRef<
     )}
     {...props}
   >
-    <SliderPrimitive.Track className="relative h-2 w-full grow overflow-hidden rounded-full bg-gray-700">
+    <SliderPrimitive.Track className="relative h-1.5 w-full grow overflow-hidden rounded-full bg-gray-700">
       <SliderPrimitive.Range className={cn("absolute h-full bg-emerald-500", rangeClassName)} />
     </SliderPrimitive.Track>
     <SliderPrimitive.Thumb
       className={cn(
-        "flex items-stretch justify-between gap-[2px] rounded-[3px] border border-primary bg-background px-[3px] h-[14px] w-[22px] -mt-[2px] ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
+        "flex items-stretch justify-between gap-[1.5px] rounded-[2px] border border-primary bg-background px-[2px] h-[12px] w-[18px] -mt-[1px] ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
         thumbClassName
       )}
     >
-      <span className="block w-px self-center h-[9px] bg-primary/60" />
-      <span className="block w-px self-center h-[9px] bg-primary/60" />
-      <span className="block w-px self-center h-[9px] bg-primary/60" />
+      <span className="block w-px self-center h-[7px] bg-primary/60" />
+      <span className="block w-px self-center h-[7px] bg-primary/60" />
+      <span className="block w-px self-center h-[7px] bg-primary/60" />
     </SliderPrimitive.Thumb>
   </SliderPrimitive.Root>
 ))
